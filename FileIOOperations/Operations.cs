2e439b315e39db00bc28eb75997ba2f2fd87be85
naveenveera789 <naveenveera789@gmail.com>
2021-12-07 12:02:01 +0530

    [naveenveera789] add : The Files I/O Operations program is done

---
+++ FileIOOperations/Operations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace SystemIO
+{
+    public class Operations
+    {
+        public void FileExists()
+        {
+            string path = @"C:\Users\navee\OneDrive\Documents\Bridgelabz practice\FileIOOperations\FileIOOperations\Files\Example.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+            Console.WriteLine();
+        }
+        public void ReadAllLines()
+        {
+            string path = @"C:\Users\navee\OneDrive\Documents\Bridgelabz practice\FileIOOperations\FileIOOperations\Files\Example.txt";
+            string[] lines;
+            lines = File.ReadAllLines(path);
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+            Console.WriteLine();
+        }
+        public void ReadAllText()
+        {
+            string path = @"C:\Users\navee\OneDrive\Documents\Bridgelabz practice\FileIOOperations\FileIOOperations\Files\Example.txt";
+            string lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+            Console.WriteLine();
+        }
+        public void FileCopy()
+        {
+            string path = @"C:\Users\navee\OneDrive\Documents\Bridgelabz practice\FileIOOperations\FileIOOperations\Files\Example.txt";
+            string copyPath = @"C:\Users\navee\OneDrive\Documents\Bridgelabz practice\FileIOOperations\FileIOOperations\Files\NewExample.txt";
+            File.Copy(path, copyPath);
+            Console.WriteLine();
+        }
+        public void DeleteFile()
+        {
+            string path = @"C:\Users\navee\OneDrive\Documents\Bridgelabz practice\FileIOOperations\FileIOOperations\Files\Example.txt";
+            File.Delete(path);
+            Console.WriteLine();
+        }
+    }
+}
